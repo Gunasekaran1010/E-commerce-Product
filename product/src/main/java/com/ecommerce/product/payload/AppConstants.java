4affdf86a29108ad96c9be573710e5d54d3a22bc
Gunasekaran P <142646139+Gunasekaran1010@users.noreply.github.com>
2024-03-25 13:13:22 +0530

    Update AppConstants.java

--- product/src/main/java/com/ecommerce/product/payload/AppConstants.java
+++ product/src/main/java/com/ecommerce/product/payload/AppConstants.java
@@ -2,7 +2,7 @@ package com.ecommerce.product.payload;
 
 public class AppConstants {
     public  static  final  String PAGE_NUMBER_STRING = "1";
-    public  static  final  String PAGE_SIZE_STRING = "2";
+    public  static  final  String PAGE_SIZE_STRING = "5";
     public  static  final  String  SORT_BY_STRING = "productId";
 
     public  static  final  String SORT_DIR_STRING = "asc";
